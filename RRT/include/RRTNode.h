848c5db3c43d999c176a7604fb1b2b9dc558f1b1
shlok <ssagarwal@wpi.edu>
2017-03-18 04:57:04 -0400

    minor changes, still deciding approach

--- RRT/include/RRTNode.h
+++ RRT/include/RRTNode.h
@@ -4,21 +4,29 @@
 class RRTNode
 {
 public:
+    /*
     std::vector<float> config;
     void storeconfig(std::vector<float> _config); // Function should store configuration in a 2D vector array
     //std::vector<> getparent(std::vector<float> _config); // Function should return parent for particular parent. Return -1 for non matching config file
-};
-class NodeTree: public RRTNode
-{
-public:
-    struct Node
-    {
-
-    };
     void storeNode(Node _node);
     bool addNode(Node _node);
     bool deleteNode(Node _node);
     Node getNode(int _index);
     std::vector<Node> getPath(int _index);
+    struct Node
+    {
+
+    }; */
+
+    typedef std::vector<float> config;
+    typedef std::vector<config> Tree;
+    Tree T;
+    Tree buildRRT(config startconfig);
+    RRTNode();
+    config sampleRandomConfig();
+
+
+
+
 
 };
