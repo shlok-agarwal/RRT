252c24baff08d1c274f4cec60f62a0b03627cbf3
shlok <ssagarwal@wpi.edu>
2017-03-21 21:26:59 -0400

    node pointer changes, but the step size process is not good. joint dof limits error

--- rrtplugin/include/NodeTree.h
+++ rrtplugin/include/NodeTree.h
@@ -9,18 +9,18 @@ using namespace std;
 class NodeTree
 {
 public:
-    std::vector<RRTNode> Nodes;
+    std::vector<RRTNode *> vecNodes;
 
     NodeTree()
     {
-        Nodes={};
+        vecNodes={}; // do i need this?
         cout<<"object created"<<endl;
     }
 
-    void addNode(std::vector<double> _config,long _self_id,long _parent_id)
+    void addNode(std::vector<double>& _config,long _self_id,long _parent_id)
     {
 
-        Nodes.push_back(RRTNode(_config,_self_id,_parent_id));
+        vecNodes.push_back(new RRTNode(_config,_self_id,_parent_id));
     }
     bool deleteNode(long self_id)
     {
@@ -30,15 +30,15 @@ public:
     }
     void printNodetree()
     {
-        for (ulong i = 0; i < Nodes.size(); ++i) {
+        for (ulong i = 0; i < vecNodes.size(); ++i) {
 
-            cout<<Nodes.at(i).self_id<<"    ";
-            cout<<Nodes.at(i).parent_id<<endl;
+            cout<<vecNodes.at(i)->self_id<<"    ";
+            cout<<vecNodes.at(i)->parent_id<<endl;
         }
     }
     int getNodeSize()
     {
-        return Nodes.size();
+        return vecNodes.size();
     }
 
     pair<long,vector<double>> nearestNeighbor(std::vector<double> _config)
@@ -47,13 +47,13 @@ public:
                               long ID=0;
                               double dist=0;
                               double closest=std::numeric_limits<double>::max(); // max value of double
-                              for (ulong i = 0; i < Nodes.size(); ++i) {
-        dist=Nodes.at(i).calcDistance(_config);
+                              for (ulong i = 0; i < vecNodes.size(); ++i) {
+        dist=vecNodes.at(i)->calcDistance(_config);
         if(dist<closest)
         {
             closest=dist;
-            nearconfig=Nodes.at(i).config;
-            ID=Nodes.at(i).self_id;
+            nearconfig=vecNodes.at(i)->config;
+            ID=vecNodes.at(i)->self_id;
         }
     }
 
--- rrtplugin/include/RRT.h
+++ rrtplugin/include/RRT.h
@@ -49,12 +49,21 @@ public:
         for (uint i = 0; i < jindex.size(); ++i) {
             if(jcircular.at(i)==1)
             {
-                qmin.at(i)=-3.14; qmax.at(i)=3.14;
+                qmin.at(i)=-2*3.14; qmax.at(i)=2*3.14;
             }
         }
-        /*for (uint i = 0; i < jindex.size(); ++i) {
+        cout<<"index"<<endl;
+        for (uint i = 0; i < jindex.size(); ++i) {
+            cout<<"     "<<jindex.at(i)<<endl;
+        }
+        cout<<"circular"<<endl;
+        for (uint i = 0; i < jcircular.size(); ++i) {
+            cout<<"     "<<jcircular.at(i)<<endl;
+        }
+        cout<<"ranges"<<endl;
+        for (uint i = 0; i < jindex.size(); ++i) {
             cout<<qmin.at(i)<<"     "<<qmax.at(i)<<endl;
-        }*/
+        }
         cout<<"Constructor Initialized"<<endl;
     }
     vector<double> sampleRandomConfig()
@@ -72,7 +81,7 @@ public:
         }*/
         return config;
     }
-    bool isCollision(vector<double> config)
+    bool isCollision(vector<double> config)  // change this function. use collision checker online example :: IMPORTANT
     {
         robot->SetActiveDOFValues(config);
         if(robot->CheckSelfCollision())
@@ -85,22 +94,23 @@ public:
         }
         else return false;
     }
-    bool buildRRT(NodeTree Nodes) // change return type later
+    bool buildRRT(NodeTree &NTree) // ask
     {
-        long maxiterations=5;
+        long maxiterations=50000;
         vector<double> config;
-        Nodes.addNode(startConfig,1,0);
-        Nodes.printNodetree();
+
+        NTree.addNode(startConfig,1,0); // ask
+        //NTree.printNodetree();
+        cout<<"about to print tree \n";
         int state=0;
-        pair<int,NodeTree> p;
-        p.second=Nodes;
+
         for (long i = 0; i < maxiterations; ++i) {
             config=sampleRandomConfig();
-            p=extend(p.second,config);
-            state=p.first; // maybe change this to pointer
-            cout<<"state:   "<<state<<endl;
-            Nodes=p.second;
-            cout<<"iteration"<<endl;
+            state=extend(NTree,config);  //ask
+
+            //  cout<<"state:   "<<state<<endl;
+
+            //   cout<<"iteration"<<endl;
             if(state==1)
             {
                 break;
@@ -114,20 +124,20 @@ public:
         }
         else return false;
     }
-    pair<int,NodeTree> extend(NodeTree Nodes,vector<double> configrand)
+    int extend(NodeTree &NTree,vector<double> configrand)
     {
 
-        cout<<"extend entered"<<endl;
-        Nodes.printNodetree();
+        // cout<<"extend entered"<<endl;
+        //NTree.printNodetree();
         vector<double> confignear;
         long tempID;
-        confignear=Nodes.nearestNeighbor(configrand).second;
-        tempID=Nodes.nearestNeighbor(configrand).first;
+        confignear=NTree.nearestNeighbor(configrand).second; // make change here ASK
+        tempID=NTree.nearestNeighbor(configrand).first;
         vector<double> configvector;
         for (uint i = 0; i < confignear.size(); ++i) {
             configvector.push_back(configrand[i]-confignear[i]);
         }
-
+        /*          Old Method
         double uvector=0;
         // find unit vector
         for (uint i = 0; i < configvector.size(); ++i) {
@@ -137,7 +147,7 @@ public:
         uvector=int(sqrt(uvector));
         for (uint i = 0; i < configvector.size(); ++i) {
             configvector[i]=configvector[i]/uvector;
-           // cout<<"config vector at :"<<i<<"  = "<<configvector[i]<<"   "<<confignear[i]<<"   "<<configrand[i]<<endl;
+            // cout<<"config vector at :"<<i<<"  = "<<configvector[i]<<"   "<<confignear[i]<<"   "<<configrand[i]<<endl;
 
         }
         // uvector - number of steps
@@ -154,29 +164,223 @@ public:
                 confignew.push_back(confignear[j]+configvector[j]);
                 if(confignew[j]==goalConfig[j]) {k++;}
             }
+
+
+        // New Method
+
+        float resolution=0.1;
+        float r;
+        vector<vector<double>> s;
+        s.resize(confignear.size());
+        for (int i = 0; i < confignear.size(); ++i) {
+            cout<<"here1"<<endl;
+            cout<<int(fabs(configvector[i]/resolution))<<endl;
+            if(int(fabs(configvector[i]/resolution))>=1)
+            {
+            if(configvector[i]>0)
+            {
+                r=resolution;
+            }
+            else r=-resolution;
+            cout<<"here2"<<endl;
+            s.at(i).assign(int(fabs(configvector[i]/resolution)),r);
+            s.at(i).assign(7,0.4);
+            cout<<"here3"<<endl;
+            s.at(i).push_back(fmod(configvector[i],resolution));
+            cout<<"here4"<<endl;
+            }
+            else s.at(i).push_back(configvector[i]);
+
+        }
+        // find largest number
+*/
+        // determine number of times to run each step
+        float resolution=0.1;
+        vector<int> n;
+        for (uint i = 0; i < confignear.size(); ++i) {
+            if(fmod(configvector[i],resolution)==0)
+                n.push_back(int(fabs(configvector[i]/resolution)));
+            else
+                n.push_back(int(fabs(configvector[i]/resolution))+1);
+        }
+        int big = 0;
+
+        for(uint i=0;i<confignear.size();i++)
+        {
+            if(n.at(i)>big)
+                big=n.at(i);
+        }
+        //  cout << "The biggest number is: " << big << endl;
+
+
+        vector<int> dir;
+        for (uint i = 0; i < confignear.size(); ++i) {
+            if(configvector[i]>0)
+            {
+                dir.push_back(1);
+            }
+            else
+                dir.push_back(-1);
+        }
+        uint k=0;
+        for (int i = 0; i < big; ++i) {
+            vector<double> confignew;
+            k=0;
+
+            // joint 1 conditions
+            if(i<n.at(0)-1)
+            {
+                confignew.push_back(confignear[0]+dir[0]*resolution);
+            }
+            else if(i==n.at(0))
+            {
+                if(fmod(configvector[i],resolution)==0)
+                {
+                    confignew.push_back(confignear[0]+resolution);
+                }
+                else confignew.push_back(fmod(configvector[i],resolution));
+            }
+            else
+            {
+                confignew.push_back(configrand[0]);
+            }
+
+
+            // joint 2 conditions
+            if(i<n.at(1)-1)
+            {
+                confignew.push_back(confignear[1]+dir[1]*resolution);
+            }
+            else if(i==n.at(1))
+            {
+                if(fmod(configvector[i],resolution)==0)
+                {
+                    confignew.push_back(confignear[1]+resolution);
+                }
+                else confignew.push_back(fmod(configvector[i],resolution));
+            }
+            else
+            {
+                confignew.push_back(configrand[1]);
+            }
+
+            // joint 3 conditions
+            if(i<n.at(2)-1)
+            {
+                confignew.push_back(confignear[2]+dir[2]*resolution);
+            }
+            else if(i==n.at(2))
+            {
+                if(fmod(configvector[i],resolution)==0)
+                {
+                    confignew.push_back(confignear[2]+resolution);
+                }
+                else confignew.push_back(fmod(configvector[i],resolution));
+            }
+            else
+            {
+                confignew.push_back(configrand[2]);
+            }
+
+            // joint 4 conditions
+            if(i<n.at(3)-1)
+            {
+                confignew.push_back(confignear[3]+dir[3]*resolution);
+            }
+            else if(i==n.at(3))
+            {
+                if(fmod(configvector[i],resolution)==0)
+                {
+                    confignew.push_back(confignear[3]+resolution);
+                }
+                else confignew.push_back(fmod(configvector[i],resolution));
+            }
+            else
+            {
+                confignew.push_back(configrand[3]);
+            }
+
+            // joint 5 conditions
+            if(i<n.at(4)-1)
+            {
+                confignew.push_back(confignear[4]+dir[4]*resolution);
+            }
+            else if(i==n.at(4))
+            {
+                if(fmod(configvector[i],resolution)==0)
+                {
+                    confignew.push_back(confignear[4]+resolution);
+                }
+                else confignew.push_back(fmod(configvector[i],resolution));
+            }
+            else
+            {
+                confignew.push_back(configrand[4]);
+            }
+
+            // joint 6 conditions
+            if(i<n.at(5)-1)
+            {
+                confignew.push_back(confignear[5]+dir[5]*resolution);
+            }
+            else if(i==n.at(5))
+            {
+                if(fmod(configvector[i],resolution)==0)
+                {
+                    confignew.push_back(confignear[5]+resolution);
+                }
+                else confignew.push_back(fmod(configvector[i],resolution));
+            }
+            else
+            {
+                confignew.push_back(configrand[5]);
+            }
+
+            // joint 7 conditions
+            if(i<n.at(6)-1)
+            {
+                confignew.push_back(confignear[6]+dir[6]*resolution);
+            }
+            else if(i==n.at(6))
+            {
+                if(fmod(configvector[i],resolution)==0)
+                {
+                    confignew.push_back(confignear[6]+resolution);
+                }
+                else confignew.push_back(fmod(configvector[i],resolution));
+            }
+            else
+            {
+                confignew.push_back(configrand[6]);
+            }
+
+            for (uint j = 0; j < confignear.size(); ++j) {
+                if(confignew[i]==goalConfig[i]) k++;
+            }
+
             if(k==goalConfig.size())
             {
                 cout<<"Goal Reached"<<endl;
-                return make_pair(1,Nodes);
+                return 1;
 
             }
             else if(isCollision(confignew))
             {
-                cout<<"collision"<<endl;
-                return make_pair(0,Nodes);
+                //     cout<<"collision"<<endl;
+                return 0;
             }
             else
             {
-                Nodes.addNode(confignew,Nodes.getNodeSize()+1,tempID);
-                //cout<<"\r"<<"Added node: "<<confignew.at(0)<<"\t"<<confignew.at(1)<<"\t"<<confignew.at(2)<<"\t"<<confignew.at(3)<<"\t"<<confignew.at(4)<<"\t"<<confignew.at(5)<<"\t"<<confignew.at(6)<<"\t"<<Nodes.getNodeSize()<<flush;
-                cout<<"Added node: "<<confignew.at(0)<<"\t"<<confignew.at(1)<<"\t"<<confignew.at(2)<<"\t"<<confignew.at(3)<<"\t"<<confignew.at(4)<<"\t"<<confignew.at(5)<<"\t"<<confignew.at(6)<<"\t"<<Nodes.getNodeSize()<<endl;
-                Nodes.printNodetree();
-                tempID=Nodes.getNodeSize();
+                NTree.addNode(confignew,NTree.getNodeSize()+1,tempID); // ask
+                cout<<"\r"<<"Added node: "<<confignew.at(0)<<"\t"<<confignew.at(1)<<"\t"<<confignew.at(2)<<"\t"<<confignew.at(3)<<"\t"<<confignew.at(4)<<"\t"<<confignew.at(5)<<"\t"<<confignew.at(6)<<"\t"<<NTree.getNodeSize()<<flush;
+                //cout<<"Added node: "<<confignew.at(0)<<"\t"<<confignew.at(1)<<"\t"<<confignew.at(2)<<"\t"<<confignew.at(3)<<"\t"<<confignew.at(4)<<"\t"<<confignew.at(5)<<"\t"<<confignew.at(6)<<"\t"<<NTree.getNodeSize()<<endl;
+                //NTree.printNodetree(); // ask
+                tempID=NTree.getNodeSize();
                 confignear=confignew;
             }
 
         }
-        return make_pair(0,Nodes);
+        return 0;
 
     }
 
